6b8d731a54765f41a73272e3fae8269a17332a3c
Lalana <lalana.hammer@regionh.dk>
2025-05-10 07:55:13 +0200

    opdater image

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ st.write("This is a Streamlit app using S2I.")
 
 # Load and display image
 image = Image.open("media/redhat.jpg")
-st.image(image, caption="This is a Red Hat OpenShift Container Platform", use_column_width=True)
+st.image(image, caption="This is a Red Hat OpenShift Container Platform", use_container_width=True)
