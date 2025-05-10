6fca06d4581460122e99eb344427f4cd21ef65d1
Lalana <lalana.hammer@regionh.dk>
2025-05-10 18:53:02 +0200

    opdater

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import streamlit as st
 from PIL import Image
+import random
+import time
 
+# Set the page configuration
 
-st.title("Unix Red Hat OpenShift Container Platform!")
-st.write("This is a Streamlit app using S2I.")
-
+st.markdown(
+    "<h2 style='text-align: center;'>This is a Streamlit app using S2I</h2>",
+    unsafe_allow_html=True)
 
 # Load and display image
-image = Image.open("media/redhat.jpg")
-st.image(image, caption="This is a Red Hat OpenShift Container Platform", use_container_width=True)
+image = Image.open("media/ocp.jpg")
+st.image(image, caption="Genereat picture from AI", use_container_width=True)
+
+st.write("This is a simple Streamlit app that demonstrates the use of S2I (Source-to-Image) to build and deploy a containerized application on OpenShift. Created by Michael & Lalana.")
+#st.image("media/penguin-dance.gif")
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("media/penguin-dance.gif", use_container_width=True)
