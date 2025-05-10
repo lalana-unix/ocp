b8dac83fab3dce74c08259654d5a2e3308bccb52
Lalana <lalana.hammer@regionh.dk>
2025-05-10 07:47:18 +0200

    redhat picture

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from PIL import Image
 
 st.title("Hello from OpenShift!")
 st.write("This is a Streamlit app using S2I.")
+
+
+# Load and display image
+image = Image.open("media/redhat.jpg")
+st.image(image, caption="This is a Red Hat OpenShift Container Platform", use_column_width=True)
