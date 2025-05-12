8a367629de8c225f5c7984b221e62082b007e331
Lalana <lalana.hammer@regionh.dk>
2025-05-12 12:32:13 +0200

    ændret content

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ st.markdown(
     unsafe_allow_html=True)
 
 # Load and display image
-image = Image.open("media/ocp.jpg")
-st.image(image, caption="Genereat picture from AI", use_container_width=True)
+#image = Image.open("media/ocp.jpg")
+#st.image(image, caption="Genereat picture from AI", use_container_width=True)
 
 st.write("This is a simple Streamlit app that demonstrates the use of S2I (Source-to-Image) to build and deploy a containerized application on OpenShift.")
 #st.image("media/penguin-dance.gif")
