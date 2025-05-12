1b0cd7853f657ad2dcca9b562169d979d4c1452a
Lalana <lalana.hammer@regionh.dk>
2025-05-12 14:34:45 +0200

    docker file image

--- appdocker.py
+++ appdocker.py
@@ -6,7 +6,7 @@ import time
 # Set the page configuration
 
 st.markdown(
-    "<h2 style='text-align: center;'>This is a Streamlit app using Dockerfile</h2>",
+    "<h2 style='text-align: center;'>This is a Streamlit app using Dockerfiel</h2>",
     unsafe_allow_html=True)
 
 # Load and display image
@@ -14,7 +14,7 @@ st.markdown(
 #st.image(image, caption="Genereat picture from AI", use_container_width=True)
 
 st.write("This is a simple Streamlit app that demonstrates the use of S2I (Source-to-Image) to build and deploy a containerized application on OpenShift.")
-#st.image("media/happy-feet-penguins.gif")
+#st.image("media/penguin-dance.gif")
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
-    st.image("media/penguin-dance.gif", use_container_width=True)
+    st.image("media/happy-feet-penguins.gif", use_container_width=True)
